5734c361eded7e71ea2256411f131b450f614f20
kranthilakum <kranthilakum+code@gmail.com>
2020-11-24 13:01:27 +0530

    Add Search Box and Movies List Heading

--- src/App.js
+++ src/App.js
@@ -2,29 +2,37 @@ import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./style.css";
 import MovieList from "./MovieList";
+import MovieListHeading from "./MovieListHeading";
+import SearchBox from "./SearchBox";
 
 const API_KEY = process.env.REACT_APP_OMDB_API_KEY;
 
 const App = () => {
   const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
 
-	const getMovieRequest = async () => {
-		const url = `https://www.omdbapi.com/?s=star%20wars&apikey=6b37a65b`;
+  const getMovieRequest = async () => {
+    const url = `https://www.omdbapi.com/?s=star%20wars&apikey=6b37a65b`;
 
-		const response = await fetch(url);
-		const responseJson = await response.json();
+    const response = await fetch(url);
+    const responseJson = await response.json();
 
-		if (responseJson.Search) {
-			setMovies(responseJson.Search);
-		}
-	};
+    if (responseJson.Search) {
+      setMovies(responseJson.Search);
+    }
+  };
 
-	useEffect(() => {
-		getMovieRequest();
-	}, []);
+  useEffect(() => {
+    // the API call only happens when the app loads for the first time
+    getMovieRequest();
+  }, []);
 
   return (
     <div className="container-fluid movie-app">
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading="Movies" />
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+      </div>
       <div className="row">
         <MovieList movies={movies} />
       </div>
